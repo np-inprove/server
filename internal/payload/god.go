907c71544fe536d31a130be0046f0ad6ad1e5aac
Qin Guan <qinguan20040914@gmail.com>
2023-05-29 16:43:14 +0800

    fix: use fqdn validator

--- internal/payload/god.go
+++ internal/payload/god.go
@@ -3,6 +3,15 @@ package payload
 import (
 	"github.com/gookit/validate"
 	"net/http"
+	"regexp"
+)
+
+const (
+	fqdnRegexStringRFC1123 = `^([a-zA-Z0-9]{1}[a-zA-Z0-9-]{0,62})(\.[a-zA-Z0-9]{1}[a-zA-Z0-9-]{0,62})*?(\.[a-zA-Z]{1}[a-zA-Z0-9]{0,62})\.?$` // From go-playground/validator, same as hostnameRegexStringRFC1123 but must contain a non numerical TLD (possibly ending with '.')
+)
+
+var (
+	fqdnRegexRFC1123 = regexp.MustCompile(fqdnRegexStringRFC1123)
 )
 
 type Institution struct {
@@ -20,10 +29,22 @@ func (i Institution) Render(_ http.ResponseWriter, _ *http.Request) error {
 type CreateInstitutionRequest struct {
 	Name          string `json:"name" validate:"required"`
 	ShortName     string `json:"short_name" validate:"required|alphaDash"`
-	AdminDomain   string `json:"admin_domain,omitempty" validate:"required|dnsName"`
-	StudentDomain string `json:"student_domain,omitempty" validate:"required|dnsName"`
+	AdminDomain   string `json:"admin_domain,omitempty" validate:"required|fqdn"`
+	StudentDomain string `json:"student_domain,omitempty" validate:"required|fqdn"`
 }
 
 func (c CreateInstitutionRequest) Validate() *validate.Validation {
-	return validate.Struct(c)
+	v := validate.Struct(c)
+	v.AddValidator("fqdn", func(val interface{}) bool {
+		s, ok := val.(string)
+		if !ok {
+			return false
+		}
+
+		if s == "" {
+			return false
+		}
+
+		return fqdnRegexRFC1123.MatchString(s)
+	})
 }
