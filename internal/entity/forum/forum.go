a0bfaa964dc649e3882c383210b89c5b7d6f7ab6
Yun-E <tye9793@gmail.com>
2023-07-02 12:50:18 +0800

    feat: remove forum roles

--- internal/entity/forum/forum.go
+++ internal/entity/forum/forum.go
@@ -1,33 +1,5 @@
 package forum
 
-import "errors"
-
-type Role string
-
-func (Role) Values() (kinds []string) {
-	for _, s := range Roles {
-		kinds = append(kinds, string(s))
-	}
-	return
-}
-
-func (fr Role) Validate() error {
-	switch fr {
-	case RoleOwner, RoleEducator, RoleMember:
-		return nil
-	default:
-		return errors.New("group role is not valid")
-	}
-}
-
-var (
-	Roles = []Role{RoleOwner, RoleEducator, RoleMember}
-
-	RoleOwner    Role = "owner"
-	RoleEducator Role = "educator"
-	RoleMember   Role = "member"
-)
-
 type Options struct {
 	Name        string
 	ShortName   string
